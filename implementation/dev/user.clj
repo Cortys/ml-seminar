4085bc59fc96397072ddefb5c828fb6a023e5417
Clemens Damke <admin@cortys.de>
2019-07-05 14:34:52 +0200

    added cosine bar graph generation

--- implementation/dev/user.clj
+++ implementation/dev/user.clj
@@ -5,6 +5,7 @@
             [clojure.stacktrace :refer :all]
             [clojure.repl :refer :all :exclude [root-cause]]
             [clojure.pprint :refer :all]
+            [clojure.string :as str]
             [proto-repl.saved-values]
             [loom.graph :as g]
             [loom.attr :as ga]
@@ -21,6 +22,37 @@
 (defn refresh-all
   []
   (ctnr/refresh-all :after 'user/start))
+
+(defmacro save
+  [type & args]
+  `(let [res# (~type ~@args)]
+     (cond
+       (g/graph? res#)
+       (dot res# ~(str "../paper/data/generated/" type ".dot"))
+       (string? res#)
+       (spit ~(str "../paper/data/generated/" type ".svg") res#))))
+
+(defn sample
+  [f start end step]
+  (map f (range start end step)))
+
+(defn svg-bars
+  [vals]
+  (let [spacing 5
+        low (apply min 0 vals)
+        high (apply max 0 vals)
+        width (* spacing (count vals))
+        height (- high low)
+        start (str "<svg height=\"" height "\" width=\"" width "\">")
+        end "</svg>"
+        bars (map-indexed (fn [i v]
+                            (let [x (+ (* spacing i) 2)]
+                              (str "<line x1=\"" x "\" y1=\"" high "\" x2=\"" x "\" y2=\"" (- high v) "\""
+                                   "style=\"stroke:rgb(0,0,0);stroke-width:3;\" />")))
+                          vals)]
+    (str start (str/join "\n" bars) end)))
+
+;; Generated:
 
 (defn two-cluster
   ([]
@@ -45,8 +77,30 @@
   []
   (two-cluster 50 150 0.02))
 
-(defmacro save
-  [type & args]
-  `(dot (~type ~@args) ~(str "../paper/data/generated/" type ".dot")))
+(defn save-graphs
+  []
+  (save small-two-cluster))
 
-(save small-two-cluster)
+(defn cos-sample
+  [n freq]
+  (sample #(* 200 (- (Math/cos (+ (* 2 Math/PI freq %) 0)))) 0 1 (/ 1 n)))
+
+(defn cos-bars-0
+  []
+  (svg-bars (map #(+ % 100) (cos-sample 150 0))))
+
+(defn cos-bars-1
+  []
+  (svg-bars (map #(- % 10) (cos-sample 150 0.5))))
+
+(defn cos-bars-10
+  []
+  (svg-bars (map #(- % 10) (cos-sample 150 5))))
+
+(defn save-bars
+  []
+  (save cos-bars-0)
+  (save cos-bars-1)
+  (save cos-bars-10))
+
+(save-bars)
